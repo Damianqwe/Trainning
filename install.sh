214274bc6f4fc28b2b653331d80184c29218b478
Damianqwe <damianslowi@gmail.com>
2019-03-13 10:11:42 +0000

    added install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#Create spring user
+sudo useradd -m -s /bin/bash spring
+#install java + git +maven
+sudo apt install -y openjdk-8-jre openjdk-8-jdk git
+sudo apt install -y maven
+sudo cp spring.service /etc/systemd/system
